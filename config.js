84ece86c6bc16538b779758c6d1e5bde45075456
Priyesh99989 <priyeshrachamalla13237@gmail.com>
2026-04-26 16:24:03 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const CHAT_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz1ALISMPOMVoVMPzDZVNl5FhQc2XqTmBgbUNibFTBPKfP1XhCB-262DGWWX3qxVcHY/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx1fepBm3rhiW25dH0CY3YWEiLBwW2ouS8lp-Z9WbE9diZKrZvcFel5scrBebWnYfmCNQ/exec';
+const EMAIL_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzOtwNTeKM_iys2ctaNhS7aVxYLYBekkQg7Q_N523gNZyR8XNYZeAupWb_SPZgB7Q0/exec';
+const FARMER_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzO-yhF7qow9pIvPe2RTMMOrWDp7_znsAP1gdhuUlMPXHhw-a9nwUeFRrY5m0xp33KU7g/exec';
+const YOUR_WHATSAPP = '916302588948';
